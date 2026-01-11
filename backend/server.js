297ea54e664b157660d391b04e27d9cca9b63078
unknown <raghuvarbisht@gmail.com>
2026-01-11 23:41:48 +0530

    add helmet package for secruty header

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
+// api limiter saving from  ddos , brute force
 import { apiLimiter } from "./middlewares/rateLimiter.js";
+import helmet from 'helmet'; 
 //routes
 import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
@@ -27,8 +29,8 @@ app.use(morgan("dev")); // for logging
 app.use(express.json()); // for json parse
 app.use(cors()); // for cors issue
 app.use(cookieParser());// uae for cookie
-
-
+// added security header using helmet
+app.use(helmet());
 // to apply on specific route app.use('/api/v1/product', apiLimiter);
 //apply rate limit on all api 
 app.use(apiLimiter);
